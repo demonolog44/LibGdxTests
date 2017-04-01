f72d0ae626374581232ce32f60f84f49d82377a9
HighwayToSkill <dmtry777@mail.ru>
2017-04-01 19:02:57 +0300

    First change.

--- core/src/com/mygdx/game/MyGdxTest.java
+++ core/src/com/mygdx/game/MyGdxTest.java
@@ -18,7 +18,7 @@ public class MyGdxTest extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(5, 5, 5, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
